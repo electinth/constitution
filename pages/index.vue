29087fb12306bb03c31a9353ca6d93ad5f82ebea
Th1nkK1D <witheep@gmail.com>
2020-12-01 18:44:51 +0700

    Add fontsize and lineheight in tailwind config

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
   >
     <div>
       <Logo />
-      <h1 class="font-heading">title</h1>
-      <h2 class="font-subtitle">subtitle</h2>
-      <p class="font-body">body</p>
+      <h1 class="font-heading text-48 leading-1.2">title</h1>
+      <h2 class="font-subtitle text-24">subtitle</h2>
+      <p class="font-body text-16">body</p>
       <button class="font-button rounded border">button</button>
     </div>
   </div>
