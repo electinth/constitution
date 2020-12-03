2cb114330a52977d59a14f6412bda27fc70ae254
Th1nkK1D <witheep@gmail.com>
2020-12-03 14:54:52 +0700

    Add categoryById and topicByCategoryId mock data

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,8 +1,35 @@
 import { rest } from 'msw';
 import categories from './data/categories.json';
+import categoryById from './data/category-by-id.json';
+import topicByCategoryId from './data/topic-by-category-id.json';
 
 export const handlers = [
-  rest.get('/categories', (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(categories));
+  rest.get('/categories', (_req, res, ctx) => res(ctx.json(categories))),
+
+  rest.get('/categories/:id', (req, res, ctx) =>
+    res(
+      ctx.json({
+        ...categoryById,
+        id: +req.params.id,
+      })
+    )
+  ),
+
+  rest.get('/topics', (req, res, ctx) => {
+    const categoryId = req.url.searchParams.get('category_id');
+
+    if (!categoryId) {
+      return res(
+        ctx.status(400),
+        ctx.json({ message: 'category_id parameter is required' })
+      );
+    }
+
+    return res(
+      ctx.json({
+        ...topicByCategoryId,
+        category_id: +categoryId,
+      })
+    );
   }),
 ];
