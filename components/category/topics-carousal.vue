266fcd3f1d1d7659b095c98bad517d6880d2b984
Th1nkK1D <witheep@gmail.com>
2020-12-21 16:51:52 +0700

    Add topics carousal in category page

---
+++ components/category/topics-carousal.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="flex flex-row h-32 md:h-56">
+    <div class="flex">
+      <div
+        class="flex w-8 h-8 z-10 bg-white rounded-full -mx-4 my-auto shadow text-light-gray-3 hover:text-gray-1 cursor-pointer"
+        @click="swiper.slidePrev()"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="m-auto"
+        >
+          <polyline points="15 18 9 12 15 6"></polyline>
+        </svg>
+      </div>
+    </div>
+
+    <swiper ref="swiper" class="swiper" :options="swiperOptions">
+      <swiper-slide v-for="{ id, name, thumbnail_image } in topics" :key="id">
+        <img :src="thumbnail_image" :alt="name" class="h-full" />
+      </swiper-slide>
+    </swiper>
+
+    <div class="flex">
+      <div
+        class="flex w-8 h-8 z-10 bg-white rounded-full -mx-4 my-auto shadow text-light-gray-3 hover:text-gray-1 cursor-pointer"
+        @click="swiper.slideNext()"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="m-auto"
+        >
+          <polyline points="9 18 15 12 9 6"></polyline>
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import SwiperClass, { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+import 'swiper/swiper-bundle.css';
+
+import { TopicOverview } from '~/utils/strapi';
+
+interface SwiperElement extends HTMLElement {
+  $swiper: typeof SwiperClass;
+}
+
+export default Vue.extend({
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  props: {
+    topics: {
+      type: Array as () => TopicOverview[],
+      required: true,
+    },
+  },
+  data() {
+    return {
+      swiperOptions: {
+        slidesPerView: 'auto',
+        spaceBetween: 5,
+      },
+    };
+  },
+  computed: {
+    swiper() {
+      const swipe = this.$refs.swiper as SwiperElement;
+      return swipe.$swiper;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.swiper-slide {
+  width: auto;
+}
+</style>
--- pages/category/_id.vue
+++ pages/category/_id.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="flex flex-col px-4 md:px-8 py-8 space-y-8">
+  <div
+    class="flex flex-col px-4 md:px-8 py-8 space-y-8 md:space-y-8 max-w-6xl mx-auto"
+  >
     <!-- Heading -->
-    <section class="text-center mb-12 max-w-2xl m-auto">
-      <Heading1 class="font-black my-12">{{ category.name }}</Heading1>
+    <section class="text-center my-8 md:my-12 mx-auto max-w-2xl space-y-12">
+      <Heading1 class="font-black">{{ category.name }}</Heading1>
       <Subtitle2 class="font-semibold">{{ category.content }}</Subtitle2>
     </section>
     <!-- End of Heading -->
@@ -13,12 +15,19 @@
       :key="subcategory.id"
       class="flex flex-col space-y-8"
     >
-      <div class="px-4 md:px-6 space-y-6">
-        <Heading4 class="font-black">{{ subcategory.name }}</Heading4>
-        <Paragraph2>{{ subcategory.content }}</Paragraph2>
-        <Label2 class="font-semibold">
-          {{ subcategory.topics.length }} posts
-        </Label2>
+      <div
+        class="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6"
+      >
+        <div class="px-4 md:px-6 space-y-6 md:max-w-sm">
+          <Heading4 class="font-black">{{ subcategory.name }}</Heading4>
+          <Paragraph2>{{ subcategory.content }}</Paragraph2>
+          <div>
+            <Label2 class="font-semibold">
+              {{ subcategory.topics.length }} posts
+            </Label2>
+          </div>
+        </div>
+        <TopicsCarousal :topics="subcategory.topics" class="flex-1" />
       </div>
       <SectionDivider />
     </section>
@@ -35,7 +44,7 @@ import { getCategoryById } from '~/utils/strapi';
 
 export default Vue.extend({
   async asyncData() {
-    const category = await getCategoryById(1);
+    const category = await getCategoryById('1');
 
     return { category };
   },
