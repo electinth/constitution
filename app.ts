96e741fe9e7f6a0246634ade5c5a20757be4d081
Thoritie <thohirh9@gmail.com>
2021-01-29 16:31:47 +0700

    :zap: #7 Run get constition data :snowman

--- app.ts
+++ app.ts
@@ -5,14 +5,19 @@ import { extractFromCsv as extractConstitutionFromCsv } from './extractors/const
 import { extractFromCsv as extractCategoryFromCsv } from './extractors/overview/category-row.ts';
 import { transform } from './transformers/overview/overview.ts';
 import { loadToJson } from './loaders/json.ts';
+import { extractConstitution } from './extractors/constitutions/constitution-detail.ts';
+import { transform as transFormConstitutionDetail } from './transformers/constitutions/constitution-detail.ts';
+
 
 const flags = parseFlags(Deno.args);
 const outputDirectory = flags.o;
 const recordRowCsvPath = flags.recordRowCsvPath;
 const constitutionCsvPath = flags.constitutionCsvPath;
 const categoryCsvPath = flags.categoryCsvPath;
+const constitutionDetailCsvPath = flags.constitutionDetailCsvPath;
 
-loadToJson(await getOverview(), `${outputDirectory}/overview.ts`);
+loadToJson(await getOverview(), `${outputDirectory}/overview.json`);
+loadToJson(await getConstitutions(), `${outputDirectory}/constitution.json`);
 
 async function getOverview() {
   const [ recordRows, cons, cats ] = await Promise.all([
@@ -21,4 +26,9 @@ async function getOverview() {
     extractCategoryFromCsv(categoryCsvPath),
   ]);
   return transform(recordRows, cons, cats);
+}
+
+async function getConstitutions() {
+  const rawData = await extractConstitution(constitutionDetailCsvPath, constitutionCsvPath);
+  return transFormConstitutionDetail(rawData);
 }
