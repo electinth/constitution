11d19d1c929fc2608fe6f6faed3b204000ed03a9
Th1nkK1D <witheep@gmail.com>
2021-03-25 16:08:49 +0700

    Join data from json and strapi

--- utils/strapi.ts
+++ utils/strapi.ts
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import { MSW_ENDPOINT } from '../mocks/handlers';
+import constitutions from '../data/constitutions.json';
 
-export interface Section {
+interface RawSection {
   id: number;
+  section_id: string;
+}
+export interface Section {
+  id: string;
   content: string;
-  chapterId: number;
-  chapterName: string;
-  partId: number | null;
-  partName: string | null;
+  chapterId?: number;
+  chapterName?: string;
+  partId?: number;
+  partName?: string;
   amendmentIds?: number[];
 }
 
@@ -20,12 +25,17 @@ export interface Amendment {
   remark: string;
 }
 
+interface RawConstitution {
+  id: number;
+  constitution_id: string;
+  sections?: RawSection[];
+}
 export interface Constitution {
   id: string;
   name: string;
   prelude: string;
   sections: Section[];
-  amendments: Amendment[];
+  amendments?: Amendment[];
 }
 
 export interface Opinion {
@@ -43,15 +53,21 @@ export interface TopicOverview {
   thumbnail_image: string;
 }
 
-export interface Topic extends TopicOverview {
+interface BaseTopic extends TopicOverview {
   category_id: string;
   category_name: string;
   subcategory_id: number;
   subcategory_name: string;
   summary: string;
-  constitutions: Constitution[];
   options: Opinion[];
   og_image: string;
+}
+interface RawTopic extends BaseTopic {
+  constitutions: RawConstitution[];
+}
+
+export interface Topic extends BaseTopic {
+  constitutions: Constitution[];
 }
 
 export interface SubCategory {
@@ -73,6 +89,51 @@ export interface Category {
 
 const strapiEndpoint = process.env.STRAPI_ENDPOINT || MSW_ENDPOINT;
 
+const parseConstitution = ({
+  constitution_id,
+  sections,
+}: RawConstitution): Constitution | null => {
+  const constitution = constitutions.find(
+    (constitution) =>
+      constitution_id === constitution.id &&
+      sections?.length &&
+      constitution.sections.some(
+        ({ id }: Section) => id === sections[0].section_id
+      )
+  );
+
+  if (!constitution) {
+    return null;
+  }
+
+  const { id, name = '', prelude } = constitution;
+
+  return {
+    id,
+    name,
+    prelude,
+    sections: sections
+      ? sections.reduce<Section[]>((list, { section_id }) => {
+          const section = (constitution.sections as Section[]).find(
+            ({ id }) => section_id === id
+          );
+          return section ? [...list, section] : list;
+        }, [])
+      : [],
+  };
+};
+
+const parseRawTopic = ({ constitutions, ...rest }: RawTopic): Topic => ({
+  ...rest,
+  constitutions: constitutions.reduce<Constitution[]>(
+    (list, rawConstitution) => {
+      const constitution = parseConstitution(rawConstitution);
+      return constitution ? [...list, constitution] : list;
+    },
+    []
+  ),
+});
+
 const get = async <T>(path: string): Promise<T> => {
   const { data } = await axios.get(
     `${strapiEndpoint}${path}`,
@@ -91,10 +152,12 @@ const get = async <T>(path: string): Promise<T> => {
 export const getCategoryById = async (id: string): Promise<Category> =>
   (await get<Category[]>(`/categories?category_id=${id}`))[0];
 
-export const getTopicsByCategoryId = (id: string): Promise<Topic[]> =>
-  get<Topic[]>(`/topics?category_id=${id}`);
+export const getAllTopics = async (): Promise<Topic[]> => {
+  const rawTopics = await get<RawTopic[]>(`/topics`);
+  return rawTopics.map(parseRawTopic);
+};
 
-export const getAllTopics = (): Promise<Topic[]> => get<Topic[]>(`/topics`);
-
-export const getTopicById = (id: string): Promise<Topic> =>
-  get<Topic>(`/topics/${id}`);
+export const getTopicById = async (id: string): Promise<Topic> => {
+  const rawTopic = await get<RawTopic>(`/topics/${id}`);
+  return parseRawTopic(rawTopic);
+};
