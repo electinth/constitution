c7275eb1684cd565d5ef764a5c2a3e24bf4675da
Sitthikarn-karn <Sitthikarn-karn@users.noreply.github.com>
2021-03-26 09:00:44 +0000

    deploy: 905be552115e13b551496e624e3dfc919e841025

--- constitution/_nuxt/static/1616748253/categories/constitutional_court/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fconstitution\u002Fconstitution\u002F_nuxt\u002Fstatic\u002F1616748253",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategories\u002Fconstitutional_court",config:{app:{basePath:"\u002Fconstitution\u002F",assetsPath:"\u002Fconstitution\u002Fconstitution\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ constitution/_nuxt/static/1616749215/categories/executive/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fconstitution\u002Fconstitution\u002F_nuxt\u002Fstatic\u002F1616749215",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategories\u002Fexecutive",config:{app:{basePath:"\u002Fconstitution\u002F",assetsPath:"\u002Fconstitution\u002Fconstitution\u002F_nuxt\u002F",cdnURL:a}}}}(null));
