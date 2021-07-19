146cf1d6573067b0f5957cbc193cdb1d008528b1
Sitthikarn-karn <Sitthikarn-karn@users.noreply.github.com>
2021-07-19 08:19:23 +0000

    deploy: 5018555cc72723dd6e9b33f08686f14d1dbe0e79

---
+++ constitution/_nuxt/static/1626682737/categories/political_ethics/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fconstitution\u002Fconstitution\u002F_nuxt\u002Fstatic\u002F1626682737",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategories\u002Fpolitical_ethics",config:{_app:{basePath:"\u002Fconstitution\u002F",assetsPath:"\u002Fconstitution\u002Fconstitution\u002F_nuxt\u002F",cdnURL:a}}}}(null));
