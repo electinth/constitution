edcd0a5fdaef40525be1be7236a43e983886d63c
Sitthikarn-karn <Sitthikarn-karn@users.noreply.github.com>
2021-07-23 08:56:03 +0000

    deploy: 5018555cc72723dd6e9b33f08686f14d1dbe0e79

--- constitution/_nuxt/static/1626682737/categories/thai_rights_and_duties/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fconstitution\u002Fconstitution\u002F_nuxt\u002Fstatic\u002F1626682737",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategories\u002Fthai_rights_and_duties",config:{_app:{basePath:"\u002Fconstitution\u002F",assetsPath:"\u002Fconstitution\u002Fconstitution\u002F_nuxt\u002F",cdnURL:a}}}}(null));
